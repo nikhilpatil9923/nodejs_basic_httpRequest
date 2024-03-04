e2d73ed216c8101de8ff8c68fce3ca2104176e77
Nikhil <nikhilpatil50599@gmail.com>
2024-03-04 20:14:25 +0530

    crud operations and filters

--- server.js
+++ server.js
@@ -3,39 +3,6 @@ const fs = require('fs') //filesystem
 const path = require('path') //to get the file path
 const { json } = require('stream/consumers');
 const server = http.createServer((req,resp) => { 
-//EX1 inspect the request details
-    // const {
-    //     headers,
-    //     url,
-    //     method
-    // } = req;   
-    // console.log(headers, url, method)
-
-//EX2 sending responseeither  as plain text /html
-//resp.setHeader('Content-type', 'text/plain');
-// resp.setHeader('Content-type', 'text/html'); 
-// resp.end('<h1>welcome to cap learning</h1>')
-
-//EX3 -sending response in json
-// resp.setHeader('Content-type', 'application/json')
-// resp.end(JSON.stringify({
-//     'Course Name': 'Capm node learning series'
-// }))  
-
-//EX4  Doing post call
-
-// if (req.method === 'POST') {
-//     var Data = []
-//    req.on('data', (chunk) => {
-//     Data.push(chunk)
-//    }).on('end', () => {
-//     const bodyString = Buffer.concat(Data).toString()
-//     resp.end(JSON.stringify(JSON.parse(bodyString)))
-// })
-// }
-
-//storing incoming payload in file
-
 if (req.method === 'POST') { 
 let Data = []
 req.on('data', (chunk) => {
@@ -67,7 +34,7 @@ Data.push(chunk)
 })
  
 } else {
- resp.end('welcome to the Course')
+ resp.end('welcome to the Course') 
 }
 
 
